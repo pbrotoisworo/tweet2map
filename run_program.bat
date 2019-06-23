02fbe3340762954ee38b54461631d0e36a0878b4
Panji Brotoisworo <32087633+pbrotoisworo@users.noreply.github.com>
2019-06-23 09:54:24 +0800

    Add files via upload

--- run_program.bat
+++ run_program.bat
@@ -1,6 +1,7 @@
 @echo off
+call activate tweet2map
 rem [PYTHON INTERPRETER] [python file]
 rem First script is the Tweet2Map.py file which runs under Python 3
 rem Second script is the ArcPy script which needs ArcPy and Python 2
-"C:\ProgramData\Anaconda3\python.exe" "C:\Users\Panji\Documents\Python Scripts\Non-Jupyter Py Scripts\MMDA Tweet2Map\Tweet2Map.py"
-"C:\Python27\ArcGIS10.6\python.exe" "C:\Users\Panji\Documents\Python Scripts\Non-Jupyter Py Scripts\MMDA Tweet2Map\arcpy_Spatial_Join_City.py"
+python "C:\Users\Panji\Documents\Python Scripts\Projects\MMDA Tweet2Map\Tweet2Map.py"
+rem "C:\Python27\ArcGIS10.6\python.exe" "C:\Users\Panji\Documents\Python Scripts\Projects\MMDA Tweet2Map\modules\arcpy_Spatial_Join_City.py"
