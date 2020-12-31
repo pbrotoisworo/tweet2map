a4c46b84c12b56de34d1cc315b1de2e6746375ff
pbrotoisworo <panji.p.broto@gmail.com>
2021-01-01 01:55:58 +0800

    Test CSV output and spatial join.

---
+++ tests/test_SpatialJoin.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+import pandas as pd
+from src.SpatialJoin import spatial_join
+
+shp_path = r'tests\test_data\boundary_ncr.shp'
+
+def test_spatial_join():
+    
+    df = pd.read_csv(r'tests\test_data\spatial_join_test.csv')
+    df_spatial = spatial_join(df, shp_path)
+    message = f'Assertion of Spatial join failed. City column not detected. Detected columns: {df_spatial.columns}'
+    assert 'City' in df_spatial.columns, message
+
+def test_city_column_data():
+    
+    df = pd.read_csv(r'tests\test_data\spatial_join_test.csv')
+    df_spatial = spatial_join(df, shp_path)
+    
+    expected = ['Pasig City', 'Mandaluyong', 'Makati City']
+    actual = list(df_spatial['City'])
+    message = f'Test of City spatial join failed. Expected "City" column data: {expected}. Actual data: {actual}'
+    assert expected == actual, message
