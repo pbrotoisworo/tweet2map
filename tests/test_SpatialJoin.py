223c1059305a055b2686716c02ab6d9ced458b98
pbrotoisworo <panji.p.broto@gmail.com>
2021-01-01 01:59:34 +0800

    Changed relative paths to absolute paths

--- tests/test_SpatialJoin.py
+++ tests/test_SpatialJoin.py
@@ -2,18 +2,20 @@ import geopandas as gpd
 import pandas as pd
 from src.SpatialJoin import spatial_join
 
-shp_path = r'tests\test_data\boundary_ncr.shp'
+TESTS_DIR = os.path.dirname(__file__)
+shp_path = os.path.join(TESTS_DIR, 'test_data', 'boundary_ncr.shp')
+test_csv_path = os.path.join(TESTS_DIR, 'test_data', 'spatial_join_test.csv')
 
 def test_spatial_join():
     
-    df = pd.read_csv(r'tests\test_data\spatial_join_test.csv')
+    df = pd.read_csv(test_csv_path)
     df_spatial = spatial_join(df, shp_path)
     message = f'Assertion of Spatial join failed. City column not detected. Detected columns: {df_spatial.columns}'
     assert 'City' in df_spatial.columns, message
 
 def test_city_column_data():
     
-    df = pd.read_csv(r'tests\test_data\spatial_join_test.csv')
+    df = pd.read_csv(test_csv_path)
     df_spatial = spatial_join(df, shp_path)
     
     expected = ['Pasig City', 'Mandaluyong', 'Makati City']
